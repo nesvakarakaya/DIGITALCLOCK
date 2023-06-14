795e19a77e5941ea63b4a6e24f938e278fb21ae1
Nesva <nesvakarakaya@gmail.com>
2023-06-15 00:15:20 +0300

    com

--- index.js
+++ index.js
@@ -6,21 +6,19 @@ function time() {
     let h = new Date().getHours();
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
-    let ampm = "AM"
-
-    if (h > 12) {
-        h = h - 12;
-        ampm = "PM";
+    let ampm = h >= 12 ? "PM" : "AM";
+    if(h>12){
+        h = h%12
     }
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
-
+    
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
-    ampmEl, (innerText = ampm);
+    ampmEl.innerText = ampm;
     setTimeout(() => {
         time();
     }, 1000)
